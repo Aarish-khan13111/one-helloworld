0234589e31ebe2c2ad5439319f0ae855526ac63d
Aarish khan <aarishkhan13111@gmail.com>
2022-11-13 19:05:52 +0530

    added all react file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style.css";
+import Button from "./Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ textAling: "center" }}>Hellow World</h1>;
+      <Button title="App store" />
+      <Button title="Play store" />
+      {/* <button className="button">clickk</button> */}
     </div>
   );
 }
